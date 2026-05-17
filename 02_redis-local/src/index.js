05937ebe1814d2bd292b6eb254a9eeb57ba44d12
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2026-05-18 00:13:44 +0530

    started learning redis

---
+++ 02_redis-local/src/index.js
@@ -0,0 +1,31 @@
+import express from "express"
+import Redis from "ioredis"
+import mongoose from "mongoose"
+
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+app.get("/redis",async(req,res) => {
+    const reply = await redis.ping();
+
+    res.json({redis:reply})
+})
+
+app.get("/mongo",async(req,res) => {
+    const url = process.env.MONGO_URL || "mongodb://localhost:27017/redis-aur-learning"
+
+    if(mongoose.connection.readyState === 0){
+        await mongoose.connect(url)
+    }
+
+    res.json({mongo:"connected", database: mongoose.connection.name})
+})
+
+
+app.listen(PORT, ()=>{
+    console.log("Server is running on: ", PORT);
+    
+})
